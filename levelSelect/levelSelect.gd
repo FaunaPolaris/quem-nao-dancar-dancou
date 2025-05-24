295dab21e96e228badd8b39c2eb76984792b3af3
Fauna Polaris <max.maxpinheiro@gmail.com>
2025-05-24 00:27:48 -0300

    tchau cabeca-de-ovo

--- levelSelect/levelSelect.gd
+++ levelSelect/levelSelect.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 
 
-func _on_cabeca_de_ouro_mouse_entered() -> void:
+func _on_cabeca_encarnada_mouse_entered() -> void:
 	$Camera2D.move_local_x(-200)
-	$"TangaraCabeca-de-ouro/VBoxContainer".show()
+	$"TangaraCabeca-encarnada/VBoxContainer".show()
 	
-func _on_cabeca_de_ouro_mouse_exited() -> void:
+func _on_cabeca_encarnada_mouse_exited() -> void:
 	$Camera2D.move_local_x(200)
-	$"TangaraCabeca-de-ouro/VBoxContainer".hide()
+	$"TangaraCabeca-encarnada/VBoxContainer".hide()
 
 func _on_dancarino_mouse_entered() -> void:
 	$Camera2D.move_local_x(200)
