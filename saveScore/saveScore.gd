f1f01e0210c8f1edb987ba86cfb635ccefc42a6a
Fauna Polaris <max.maxpinheiro@gmail.com>
2025-05-25 10:14:11 -0300

    highscore

--- saveScore/saveScore.gd
+++ saveScore/saveScore.gd
@@ -1,7 +1,19 @@
 extends Node2D
 
+var highscore : int = 0
+
 func	_ready():
 	$Score.set_text("".join(PackedStringArray([$Score.text, str(PlayerInfo.score)])))
+	if PlayerInfo.current_bird == "dancarino" and ScoreBoard.dancarino.size():
+		highscore = ScoreBoard.dancarino[0][0]
+	if PlayerInfo.current_bird == "encarnado" and ScoreBoard.encarnado.size():
+		highscore = ScoreBoard.encarnado[0][0]
+	$selected_bird.play(PlayerInfo.current_bird)
+	$highscore.set_text("".join(PackedStringArray([$highscore.text, str(highscore)])))
+	if PlayerInfo.score > highscore:
+		$"new best".play()
+	else:
+		$normal.play()
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	PlayerInfo.player_name = new_text
